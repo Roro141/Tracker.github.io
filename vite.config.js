cca08c312e1d956d29e9f58b667c5b82a2085ff4
RohaFatima <100485343+Roro141@users.noreply.github.com>
2025-09-06 16:50:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // For a user/organization site repo named <username>.github.io, base can stay '/'.
-  // If this were a project page, you'd set: base: '/<repo-name>/'
+ 
+  base: '/'
 })
